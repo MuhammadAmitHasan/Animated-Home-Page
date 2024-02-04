f3f5511c76702971fc666d13bc86280d132f234b
MohammadAmitHasan <bhamithasan@gmail.com>
2024-02-04 12:44:32 +0600

    feat: Favicon update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
 import HomeComponent from '@/components/HomeComponent';
+import Head from 'next/head';
 
 export default function Home() {
 
   return (
     <main>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
       <HomeComponent />
     </main>
   )
